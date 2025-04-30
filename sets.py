d33b4180d569cd16b302716657234c9514962db4
AdhritShetty <adhritshetty2009@gmail.com>
2025-04-30 20:42:15 +0000

    sets

---
+++ sets.py
@@ -0,0 +1,17 @@
+my_set={1,2,34,'hello'}
+my_set.pop()
+print(my_set)
+my_set2={1,7,9,'bye'}
+print(my_set2)
+
+#intersection
+my_Set3=my_set2.intersection(my_set)
+print(my_Set3)
+
+#union
+my_Set3=my_set2.union(my_set)
+print(my_Set3)
+
+#difference
+my_Set3=my_set2.difference(my_set)
+print(my_Set3)
